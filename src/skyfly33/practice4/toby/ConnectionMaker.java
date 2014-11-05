4b9790c1bae4827c40986b8360d48d55b3738ef6
skyfly33 <skyfly33@iruen.com>
2014-11-05 21:34:21 +0900

    @practice 10 : java소스기반 datasource 추

--- src/skyfly33/practice4/toby/ConnectionMaker.java
+++
@@ -1,8 +0,0 @@
-package skyfly33.practice4.toby;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface ConnectionMaker {
-	public Connection makeNewConnection() throws SQLException, ClassNotFoundException;
-}
